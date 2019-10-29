878f0acfee89473e7f2bfecb8aff86081c62fff8
antkanfu <web.fr0nt3nd@gmail.com>
2019-10-28 17:14:18 -0700

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2d";
+ctx.lineWidth = 2.5;
 
 let paintng = false;
 
@@ -6,23 +10,32 @@ function stopPainting() {
     paintng = false;
 }
 
+function startPainting() {
+    paintng = true;
+}
+
 function onMouseMove(event){
     // console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!paintng){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown(evnet){
+function onMouseDown(event){
     paintng = true;
+
 }
 
-function onMouseUp(evnet){
-    stopPainting();
-}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
